cfdd601061cc8963a115d79a823a9a2751830143
Khushi Mehta <khushimehta459@gmail.com>
2020-06-09 23:27:41 +0530

    Trying to revise the concepts

---
+++ assignment.java
@@ -0,0 +1,16 @@
+class assignment
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        a=8;
+        a+=2345;
+        b=3;
+        a/=b;
+        c=5;
+        a%=c;
+        a*=c;
+        System.out.println(a);
+    }
+}
+        
